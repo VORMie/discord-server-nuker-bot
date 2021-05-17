5c5e2fcd3b89422dbedc790cb286e9b6cdfe30c0
Vishesh Varma <vsvarma02@gmail.com>
2021-05-17 21:02:38 +0530

    Disguise Commands

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+from discord.flags import Intents
+
+bottk="ODQzODU2NjI1MTAyMjI1NDA4.YKJ8uQ.9DweZMuEuQKQvk2CHE8TGLu6H7E"
+
+intents=discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
+client=commands.Bot(server_prefix='!', intents=intents)
+
+@client.event
+async def on_ready():
+    print("You're ready to rumble!")
+
+@client.command
+async def ping(ctx):
+    ctx.send(f'Pong! Latency is {client.latency*1000}ms')
+
+@client.command
+async def ban(ctx, member: discord.Member, Reason=None):
+    await member.ban(reason=Reason)
+    ctx.send(f"The user **{member}** has been banned!")
+
+@client.command
+async def kick(ctx, member: discord.Member, Reason=None):
+    await member.kick(reason=Reason)
+    ctx.send(f"The user **{member}** has been kicked!")
+
+client.run(bottk)
